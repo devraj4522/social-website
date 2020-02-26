c547a896b6c995fdab180ceb02e994b81a92b14c
Dev Raj <rehansingh.4522@gmail.com>
2020-02-26 14:19:45 +0530

    'Added

--- account/tests.py
+++ account/tests.py
@@ -1,10 +1,1 @@
 from django.test import TestCase
-
-# Create your tests here.
-import os
-import datetime
-import csv
-lis = [[
-1997,'Ford',"E350" ,"ac", "abs", "moon",3000.00]
-,[1999,"Chevy","Venture ","Extended Edition",4900.00],]
-print(cat)
